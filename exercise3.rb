4d387df29ae2a3dc06b7f8aefeaa3097c8aa35a6
Jack WIlde <jackwilde630@gmail.com>
2016-08-09 13:58:58 -0400

    calculate year user was born

--- exercise3.rb
+++ exercise3.rb
@@ -3,3 +3,11 @@ puts "What is your name?"
 name = gets.chomp
 
 puts "Hi #{name}!"
+
+puts "how old are you?"
+
+age = gets.chomp.to_i
+
+birth_year = Time.new.year - age
+
+puts "you were probably born in #{birth_year}"
