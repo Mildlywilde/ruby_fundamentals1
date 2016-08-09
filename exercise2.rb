1a8bf1329dd6574a4a8f51e492eaf91efca689e7
Jack WIlde <jackwilde630@gmail.com>
2016-08-09 13:41:58 -0400

    Finish exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,15 @@
+# Problem 1 - tip for a 55 dollar meal
+
+puts (55.0 / 100) * 15
+
+#Problem 2 - add a string and integer
+
+puts 'this is a string and the number ' + 1.to_s
+
+#Problem 3 - output 45628 * 7839 using string interpolation
+
+puts "the answer to 45628 times 7839 is #{45628 * 7839}"
+
+#problem 4 - the value of the expression (10 < 20 && 30 < 20) || !(10 == 11)
+
+false
